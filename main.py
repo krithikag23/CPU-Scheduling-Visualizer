c17c298bf30a626f5fb6ca0a3237214ce09dfe5f
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-04 20:29:29 +0530

    update

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from schedulers.sjf import sjf
 from schedulers.rr import rr
 from utils.visualize import gantt_chart
 from utils.metrics import calculate_metrics
+
+# Sample process list: (process_id, burst_time, arrival_time)
+processes = [
+    ('P1', 5, 0),
+    ('P2', 3, 1),
+    ('P3', 8, 2),
+    ('P4', 6, 3)
+]
